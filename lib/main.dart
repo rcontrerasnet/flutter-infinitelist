3061d935b6bae356775892deab81411971f10506
Rafael Contreras <r.contreras@Rafaels-MacBook-Pro.local>
2018-12-12 00:28:46 -0400

    Changed them primary color to white

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
@@ -23,6 +26,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ) ,
       body: _buildSuggestions(),
       );
@@ -57,9 +63,49 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: (){
+        setState(() {
+                  if(alreadySaved){
+                    _saved.remove(pair);
+                  } else{
+                    _saved.add(pair);
+                  }
+                });
+      },
     );
   }
 
+  void _pushSaved(){
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair){
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style:_biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided,),
+          );
+        },
+      ),
+    );
+  }
 }
 
 class RandomWords extends StatefulWidget {
